fadab54d749179505cabb510c5b5e1c8b9048f4f
Kaique Ferraz <kaiqueferraz97@gmail.com>
2020-09-22 00:09:03 -0300

    Projeto Criado

---
+++ src/test/java/com/kaiqueferraz/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiqueferraz.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
