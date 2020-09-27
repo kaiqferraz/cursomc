a04c51a5b8d61711f3895d1038dd99693123a0f2
Kaique Ferraz <kaiqueferraz97@gmail.com>
2020-09-27 14:06:53 -0300

    Inserindo nova Categoria com o post

--- src/main/java/com/kaiqueferraz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kaiqueferraz/cursomc/resources/CategoriaResource.java
@@ -4,14 +4,13 @@ package com.kaiqueferraz.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kaiqueferraz.cursomc.domain.Categoria;
 import com.kaiqueferraz.cursomc.services.CategoriaService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 
 
 @RestController
@@ -28,6 +27,22 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	
 	}
+
+    @RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){     //@RequestBody Json ser convertido p/ obj java automaticament.e
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+           return ResponseEntity.created(uri).build();
+	}
+
+
+
+
+
+
+
+
 	
 	
 
